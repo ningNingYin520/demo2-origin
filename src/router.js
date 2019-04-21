662c6220306d6bdd4bb29293b426a8956e7cba84
ningNingYin520 <951361193@qq.com>
2019-04-21 22:36:39 +0800

    新建项目

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from './views/Login.vue'
 import Home from './views/Home.vue'
+
 
 Vue.use(Router)
 
@@ -10,16 +12,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      children:[
+        {
+          path: 'list',
+          name: 'list',
+          component: () => import(/* webpackChunkName: "list" */ './views/List.vue')
+        },
+        {
+          path: 'user',
+          name: 'user',
+          component: () => import(/* webpackChunkName: "user" */ './views/User.vue')
+        }
+      ]
+    }
+    /*{
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import(/!* webpackChunkName: "about" *!/ './views/About.vue')
+    }*/
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button type="button" @click="reg()">登录</button>
+        <button type="button">注册</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        methods:{
+            reg () {
+                this.$router.push('/home/list')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
